34eea40a59be6afe2da0e30c53daef0590b552fc
Mateus Oliveira <mateusteknus@gmail.com>
2018-11-15 11:31:32 -0300

    ...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :login, presence: true, uniqueness: true
-    validates :password, presence: true, length: { in: 6..20 }
+    validates :password, presence: true, length: { in: 6..30}
     validates :email, presence: true
     validates :nome, presence: true
     validates :sobrenome, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -28,13 +28,6 @@ ActiveRecord::Schema.define(version: 2018_11_15_140643) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "migrations", force: :cascade do |t|
-    t.string "add_diasemana_to_horario"
-    t.string "diasemana"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "login"
     t.string "password"
